534113de1b9bc0fe5c73f8aa4963775ad54202a7
nknkget0k <a.nakajima1215@gmail.com>
2015-11-23 22:51:46 +0000

    add user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
-  def new
-    @user = User.new
+  
   def create
     @user = User.new(user_params)
     if @user.save
@@ -16,3 +15,4 @@ class UsersController < ApplicationController
                                  :password_confirmation)
   end
 end
+
